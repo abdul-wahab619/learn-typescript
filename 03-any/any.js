eaea5515083b6f5bc6df8e8658dd19e018cef476
abdul-wahab619 <awminhas619@gmail.com>
2025-10-07 13:28:05 +0500

    ts: any

---
+++ 03-any/any.js
@@ -0,0 +1,4 @@
+var data = "Hello";
+data = 42;
+data = true;
+console.log(data === null || data === void 0 ? void 0 : data.toString().toUpperCase());
---
+++ 03-any/any.ts
@@ -0,0 +1,5 @@
+let data: any = "Hello";
+data = 42;
+data = true;
+
+console.log(data?.toString().toUpperCase());
