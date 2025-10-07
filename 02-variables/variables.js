eb7b461b440e3fde7783303827b738dd36318e18
abdul-wahab619 <awminhas619@gmail.com>
2025-10-07 13:01:42 +0500

    ts: variables

---
+++ 02-variables/variables.js
@@ -0,0 +1,9 @@
+let greetings = "Hello, World!";
+let rollNumber = 66;
+let isLoggedIn = true;
+let bigNumber = 12345678901234567890n;
+console.log(greetings);
+console.log(rollNumber);
+console.log(isLoggedIn);
+console.log(bigNumber);
+export {};
---
+++ 02-variables/variables.ts
@@ -0,0 +1,13 @@
+let greetings: string = "Hello, World!";
+let rollNumber: number = 66;
+let isLoggedIn: boolean = true;
+
+let bigNumber: bigint = 12345678901234567890n;
+
+
+
+console.log(greetings);
+console.log(rollNumber);
+console.log(isLoggedIn);
+console.log(bigNumber);
+export {};
