eb6ef5f49cd4d9b732a878ea4031a12e5fc6bf9b
abdul-wahab619 <awminhas619@gmail.com>
2025-10-07 13:53:49 +0500

    ts: improve functions

--- 04-functions/function.js
+++ 04-functions/function.js
@@ -11,7 +11,20 @@ function logInUser(name, email, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
     return { name: name, email: email, isPaid: isPaid };
 }
+var getHello = function (s) {
+    return "Hello " + s;
+};
+var heros = ["thor", "spiderman", "ironman"];
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
 console.log(addTwo(5));
 console.log(getUpper("hello"));
 console.log(signUpUser("John", "john@example.com", true));
 console.log(logInUser("John", "john@example.com"));
+console.log(getHello("John"));
+console.log(heros);
+consoleError("This is an error message");
--- 04-functions/function.ts
+++ 04-functions/function.ts
@@ -14,7 +14,28 @@ function logInUser(name: string, email: string, isPaid: boolean = false) {
   return { name, email, isPaid };
 }
 
+const getHello = (s: string): string => {
+  return "Hello " + s;
+};
+
+const heros: Array<string> = ["thor", "spiderman", "ironman"];
+
+heros.map((hero): string => {
+  return `hero is ${hero}`;
+});
+
+function consoleError(errmsg: string): void {
+  console.log(errmsg);
+}
+function handleError(errmsg: string): never {
+  throw new Error(errmsg);
+}
+
 console.log(addTwo(5));
 console.log(getUpper("hello"));
 console.log(signUpUser("John", "john@example.com", true));
 console.log(logInUser("John", "john@example.com"));
+console.log(getHello("John"));
+console.log(heros);
+consoleError("This is an error message");
+console.log(handleError("This is a critical error"));
