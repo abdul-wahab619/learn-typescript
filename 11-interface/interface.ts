6ae85c6a35eec924a54846afbb769b868feadf81
abdul-wahab619 <awminhas619@gmail.com>
2025-10-08 18:00:24 +0500

    ts: interface vs alias type

--- 11-interface/interface.ts
+++ 11-interface/interface.ts
@@ -57,4 +57,32 @@ interface Add {
 const addNumbers: Add = (x, y) => x + y;
 console.log(addNumbers(5, 10)); // 15
 
+// 🧱 Difference Between interface and type:
+
+// 1. Declaration Merging: Interfaces can be merged, while type aliases cannot.
+interface Car {
+  make: string;
+}
+interface Car {
+  model: string;
+}
+const myCar: Car = { make: "Toyota", model: "Corolla" };
+// type Bike = { brand: string; };
+// type Bike = { type: string; }; // Error: Duplicate identifier 'Bike'.
+
+// 2. Extending: Both can extend, but interfaces can only extend other interfaces.
+interface Animal {
+  species: string;
+}
+interface Dog extends Animal {
+  breed: string;
+}
+const myDog: Dog = { species: "Canine", breed: "Labrador" };
+// type Cat = { species: string; };
+// type PersianCat = Cat & { color: string; };
+// const myCat: PersianCat = { species: "Feline", color: "White" };
+
+// interface Best for object shapes
+// type Best for unions, primitives, and complex types
+
 export {};
